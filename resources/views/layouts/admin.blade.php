6177d034cb068068fd7625afa29a87d6a0d8698d
Yuritzi Calixto <ramirezcalixtodiana@gmail.com>
2026-01-26 16:50:43 -0600

    Sección 3: Administrador Completo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'breadcrumbs'=>[],
+    ])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -11,14 +15,21 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        {{-- FONTAWESOME --}}
+        <script src="https://kit.fontawesome.com/5554864cfa.js" crossorigin="anonymous"></script>
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
     </head>
-    <body class="font-sans antialiased" x-data="{
+    <body class="font-sans antialiased"
+    x-data="{
     sidebarOpen: false
+    }"
+    :class="{
+        'overflow-y-hidden': sidebarOpen
     }">
 
     <div class="fixed inset-0 bg-gray-900 bg-opacity-50 z-20 sm:hidden"
@@ -36,9 +47,15 @@
 
 
 
-<div class="p-4 sm:ml-64 mt-14">
-   <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ">
-    {{ $slot }}
+<div class="p-4 sm:ml-64">
+
+    <div class="mt-14">
+        @include('layouts.partials.admin.breadcrumb')
+
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ">
+
+        {{ $slot }}
+        </div>
    </div>
 </div>
 
--- resources/views/layouts/partials/admin/sidebar.blade.php
+++ resources/views/layouts/partials/admin/sidebar.blade.php
@@ -1,3 +1,15 @@
+@php
+    $links= [
+    [
+        'icon' => 'fa-solid fa-gauge',
+        'name' => 'Dashboard',
+        'route' => route ('admin.dashboard'),
+        'active' => request()->routeIs('admin.dashboard')
+    ],
+
+];
+@endphp
+
 <aside id="logo-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-[100dvh] pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     :class="{
@@ -8,12 +20,11 @@
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
         <ul class="space-y-2 font-medium">
 {{--ITERANDO ENLACES--}}
-            {{-- @foreach ($links as $link)
+             @foreach ($links as $link)
                 @canany($link['can'] ?? [null])
 
-                    <li> --}}
-                        {{--  --}}
-                        {{-- <a href="{{ $link['route'] }}"
+                    <li>
+                        <a href="{{ $link['route'] }}"
                             class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ $link['active'] ? 'bg-gray-100' : ''}}">
 
                             <span class="inline-flex w-6 h-6 justify-center items-center">
@@ -26,7 +37,7 @@
                         </a>
                     </li>
                 @endcanany
-            @endforeach --}}
+            @endforeach
         </ul>
     </div>
 </aside>
